ad9a867a083615f71ce18b5c2d27354984abd504
Nihar Dangi <nihardangi@gmail.com>
2025-06-20 23:17:11 +0530

    added DEXEngine contract

---
+++ src/DEXEngine.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.24;
+
+import {LiquidityPool} from "./LiquidityPool.sol";
+
+contract DEXEngine {
+    /////////////////////////////////////
+    ///           Errors              ///
+    /////////////////////////////////////
+    error DEXEngine__LiquidityPoolExistsForToken();
+
+    ////////////////////////////////////
+    ///       State Variables        ///
+    ////////////////////////////////////
+    mapping(address => address) tokenToLiquidityPool;
+    mapping(address => address) liquidityPoolToToken;
+
+    /////////////////////////////////////
+    ///           Events              ///
+    /////////////////////////////////////
+    event NewLiquidityPoolCreated(address indexed token, address indexed pool);
+
+    /////////////////////////////////////
+    ///  External & Public Functions  ///
+    /////////////////////////////////////
+    function createLiquidityPool(address token) external returns (address) {
+        if (tokenToLiquidityPool[token] != address(0)) {
+            revert DEXEngine__LiquidityPoolExistsForToken();
+        }
+        LiquidityPool pool = new LiquidityPool(token);
+        tokenToLiquidityPool[token] = address(pool);
+        liquidityPoolToToken[address(pool)] = token;
+        emit NewLiquidityPoolCreated(token, address(pool));
+        return address(pool);
+    }
+
+    function getLiquidityPool(address token) external view returns (address) {
+        return tokenToLiquidityPool[token];
+    }
+
+    function getToken(address pool) external view returns (address) {
+        return liquidityPoolToToken[pool];
+    }
+}
